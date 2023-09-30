8e8305cb60a7ba8283233001994c5d01645bb9f4
Callum Moffat <smartercallum@gmail.com>
2023-09-29 21:59:15 -0400

    Copy webkit boundary exactly

--- dio/lib/src/form_data.dart
+++ dio/lib/src/form_data.dart
@@ -9,8 +9,15 @@ import 'utils.dart';
 /// A class to create readable "multipart/form-data" streams.
 /// It can be used to submit forms and file uploads to http server.
 class FormData {
-  static const String _BOUNDARY_PRE_TAG = '--WebKitFormBoundary';
-  static const _BOUNDARY_LENGTH = _BOUNDARY_PRE_TAG.length + 10;
+  static const String _BOUNDARY_PRE_TAG = '----WebKitFormBoundary';
+  static const _BOUNDARY_LENGTH = _BOUNDARY_PRE_TAG.length + 16;
+  static const _kAlphaNumericEncodingMap = [
+      0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B,
+      0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56,
+      0x57, 0x58, 0x59, 0x5A, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67,
+      0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F, 0x70, 0x71, 0x72,
+      0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x30, 0x31, 0x32,
+      0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x41, 0x42];
 
   late String _boundary;
 
@@ -61,7 +68,7 @@ class FormData {
     // Assure the boundary unpredictable and unique
     var random = Random();
     _boundary = _BOUNDARY_PRE_TAG +
-        random.nextInt(4294967296).toString().padLeft(10, '0');
+        String.fromCharCodes(Iterable.generate(16, (i) => _kAlphaNumericEncodingMap[random.nextInt(64)]));
   }
 
   /// Returns the header string for a field. The return value is guaranteed to
